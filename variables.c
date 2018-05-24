b942d20f7d2cd31b2e16ef1fde999a2d988d382b
maroline12 <micmag626@gmail.com>
2018-05-24 11:31:33 -0700

    c2

---
+++ variables.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b = 3;
+  int c;
+
+  a = 2;
+  c = a + b;
+  
+  int i;
+for(i=0; i<2; i++);
+
+  a = i;
+  a = c;
+  c = i;
+
+printf("a is %d and c is %d\n", a, c);
+}
+
